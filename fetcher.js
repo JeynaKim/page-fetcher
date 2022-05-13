96d2f6f484b3be83b48ae721bd07764a587a0a99
Jeyna Kim <cocomimusic@gmail.com>
2022-05-13 17:28:48 -0400

    Page Downloader completed

---
+++ fetcher.js
@@ -0,0 +1,23 @@
+const args = process.argv.slice(2);
+const fs = require("fs");
+const request = require("request");
+
+const pageFetcher = function (url, fileName) {
+  request(url, (error, response, body) => {
+    if (error) {
+      return console.log("error!", error);
+    }
+    // console.log(response);
+
+    fs.writeFile(fileName, body, (err) => {
+      if (err) {
+        return console.log("error!", err);
+      }
+      console.log(`Downloaded and saved ${body.length} bytes to ${fileName}.`);
+    });
+  });
+};
+
+pageFetcher(args[0], args[1]);
+
+// document.getElementById('file').files[0].size
